e4c8555d0a1a31f42d1517eb430d226ae2471c82
Egg6691 <91635113+Egg6691@users.noreply.github.com>
2025-10-18 16:21:52 -0500

    key

--- app.py
+++ app.py
@@ -1,26 +1,30 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
+app.config['BRAND'] = 'HowdyHack'
+app.config['SECRET_KEY'] = 'i like to play roblox';
 
-
+@app.context_processor
+def inject_brand():
+    return {'brand': app.config.get('BRAND', 'Your Brand')}
 @app.route('/')
 def index():
-    return render_template('index.html', active_page='home', brand='HowdyHack')
+    return render_template('index.html', active_page='home')
 
 
 @app.route('/about')
 def about():
-    return render_template('index.html', active_page='about', brand='HowdyHack')
+    return render_template('index.html', active_page='about')
 
 
 @app.route('/services')
 def services():
-    return render_template('index.html', active_page='services', brand='HowdyHack')
+    return render_template('index.html', active_page='services')
 
 
 @app.route('/contact')
 def contact():
-    return render_template('index.html', active_page='contact', brand='HowdyHack')
+    return render_template('index.html', active_page='contact')
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -32,7 +36,7 @@ def login():
         # TODO: add real authentication
         flash('Login attempt for: ' + (email or ''), 'info')
         return redirect(url_for('index'))
-    return render_template('login.html', active_page='login', brand='HowdyHack')
+    return render_template('login.html', active_page='login')
 
 
 @app.route('/signup', methods=['GET', 'POST'])
@@ -44,7 +48,7 @@ def signup():
         # TODO: create user and validate
         flash('Account created for: ' + (email or ''), 'success')
         return redirect(url_for('login'))
-    return render_template('signup.html', active_page='signup', brand='HowdyHack')
+    return render_template('signup.html', active_page='signup')
 
 
 if __name__ == '__main__':
